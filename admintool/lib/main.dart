c1d21925bca1a845a462091e2a9a26a42386c955
kandlinf <kandlinger@gmail.com>
2022-04-22 11:17:20 +0200

    Basic Firebase Authentication

--- admintool/lib/main.dart
+++ admintool/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
