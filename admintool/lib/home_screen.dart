c9038ab30b48c8af0296d33f7aed4c40466e027d
kandlinf <kandlinger@gmail.com>
2022-05-16 21:26:00 +0200

    User Profile and Roles Implementation

--- admintool/lib/home_screen.dart
+++ admintool/lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key, required this.user,}) : super(key: key);
@@ -9,14 +10,15 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    
+      return Scaffold(
       appBar: AppBar(
         title: const Text('LHM Authentication Demo'),
       ),
       body: Column(
         children: [
           Text(
-            user.email!,
+            "EMail: " + user.email! + " Id: " + user.uid,
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
           const SignOutButton(),
--- admintool/lib/main.dart
+++ admintool/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'LHM Authentication Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
