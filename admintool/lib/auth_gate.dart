86f32e710efee8657305cc3e2cb4d7cb124787bd
kandlinf <kandlinger@gmail.com>
2022-05-17 07:50:23 +0200

    Beautified Login Screen

--- admintool/lib/auth_gate.dart
+++ admintool/lib/auth_gate.dart
@@ -13,11 +13,29 @@ class AuthGate extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
-            return const SignInScreen(
+            return SignInScreen(              
+                sideBuilder: (context, constraints) {
+                  return const Padding(
+                    padding: EdgeInsets.all(20),
+                    child: AspectRatio(
+                      aspectRatio: 1,
+                      child: Image(image: AssetImage('graphics/logo.png'))
+                    ),
+                  );
+                },
+                subtitleBuilder: (context, action) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      action == AuthAction.signIn
+                          ? 'Welcome to the KID Kooperation Indoor Navigation!'
+                          : 'Welcome to the KID Kooperation Indoor Navigation!'
+                    ),
+                  );
+                },
                 providerConfigs: [EmailProviderConfiguration()]);
           }
           return HomeScreen(user: snapshot.data!);
         });
   }
 }
- 
--- admintool/lib/main.dart
+++ admintool/lib/main.dart
@@ -7,7 +7,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );  
+  );
   runApp(const MyApp());
 }
 
